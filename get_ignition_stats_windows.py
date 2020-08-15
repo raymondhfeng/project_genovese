da14c52197467b1d14a345dee2347dd1029c74d2
Raymond Feng <raymondhfeng@berkeley.edu>
2020-08-14 18:37:45 -0700

    added debugging for the ocr

--- get_ignition_stats_windows.py
+++ get_ignition_stats_windows.py
@@ -4,7 +4,9 @@ def get_stats():
 	# This is because certain libraries are not thread safe. 
 	# https://github.com/celery/celery/issues/2964	
 
-	from PIL import Image 
+	from PIL import Image
+        from PIL import ImageDraw
+        from PIL import ImageFont
 
 	import glob
 	import os
@@ -25,7 +27,7 @@ def get_stats():
 	    result.paste(pil_img, (left, top))
 	    return result
 
-	list_of_files = glob.glob('/home/pi/screenshots/') 
+	list_of_files = glob.glob('/home/pi/screenshots/*') 
 	latest_file = max(list_of_files, key=os.path.getctime)
 
 	# latest_file = '/Users/raymondfeng/Desktop/TrickyWays/cropped/chubert.jpeg'
@@ -34,9 +36,9 @@ def get_stats():
 	im = Image.open(latest_file) 
 	  
 	left = 292
-	top = 341 - 4
+	top = 341 + 3
 	right = 908
-	bottom = 443 - 4	
+	bottom = 443 + 3	
 
 	im1 = im.crop((left, top, right, bottom)) 
 
@@ -64,8 +66,11 @@ def get_stats():
 		img = 255 - img
 		cv2.imwrite(path_name, img)
 		img = Image.open(path_name)
-		num_ppl.append(pytesseract.image_to_string(img, 
-			config='--psm 7 --oem 3 -c tessedit_char_whitelist=0123456789+'))
+		result = pytesseract.image_to_string(img, config='--psm 7 --oem 3 -c tessedit_char_whitelist=0123456789+')
+                draw = ImageDraw.Draw(img)
+                font = ImageFont.truetype("/home/pi/open-sans/OpenSans-Regular.ttf", 52)
+                draw.text((0, 0),str(result),(0),font=font)
+                num_ppl.append(result)
 
 	avg_pot = []
 	avg_pot_width = 60
@@ -75,7 +80,7 @@ def get_stats():
 	for i in range(num_rows):
 		img = im.crop((left, i*y_delta,
 			left+avg_pot_width, (i+1)*y_delta))
-		img = add_margin(img, 5, 0, 5, 0, (255,255,255)) # Supposedly helps the OCR
+		img = add_margin(img, 5, 0, 5, 0, (0,0,0)) # Supposedly helps the OCR
 		path_name = os.path.join('/home/pi/pre_ocr', 
 			str(datetime.now())[10:19].replace(':','_') + '_' + str(i) + '_' + 'avg_pot.png')
 		img = img.resize((round(img.size[0]*10), round(img.size[1]*10)), Image.ANTIALIAS)
@@ -99,12 +104,12 @@ def get_stats():
 		img = add_margin(img, 5, 0, 5, 0, (0,0,0)) # Supposedly helps the OCR
 		path_name = os.path.join('/home/pi/pre_ocr', 
 			str(datetime.now())[10:19].replace(':','_') + '_' + str(i) + '_' + 'plrs_flop.png')
-		img = img.resize((round(img.size[0]*12), round(img.size[1]*12)), Image.ANTIALIAS)
+		img = img.resize((round(img.size[0]*10), round(img.size[1]*10)), Image.ANTIALIAS)
 		img.save(path_name)
 		img = cv2.imread(path_name, 0)
 		# img = cv2.filter2D(img, -1, np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]]))
 		# img = cv2.GaussianBlur(img, (3,3), 0)
-		img = cv2.erode(img, np.ones((5,5), np.uint8), iterations=2)
+		img = cv2.erode(img, np.ones((5,5), np.uint8), iterations=1)
 		thresh, img = cv2.threshold(img,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 		img = 255 - img
 		cv2.imwrite(path_name, img)
